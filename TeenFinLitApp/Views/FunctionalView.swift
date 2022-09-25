0221d9147b0309458299f950873cb3cc65585b55
Lucas Robbins <altairboard@gmail.com>
2022-09-24 20:31:10 -0600

    -Added files for MVVM layout

---
+++ TeenFinLitApp/Views/FunctionalView.swift
@@ -0,0 +1,20 @@
+//
+//  FunctionalView.swift
+//  TeenFinLitApp
+//
+//  Created by Lucas Robbins on 9/24/22.
+//
+
+import SwiftUI
+
+struct FunctionalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FunctionalView_Previews: PreviewProvider {
+    static var previews: some View {
+        FunctionalView()
+    }
+}
