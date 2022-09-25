4f47cfc9b384b886b75fd24c3c595c89f6390945
Lucas Robbins <altairboard@gmail.com>
2022-09-24 20:23:21 -0600

    Initial Commit

---
+++ TeenFinLitApp/TeenFinLitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeenFinLitAppApp.swift
+//  TeenFinLitApp
+//
+//  Created by Lucas Robbins on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct TeenFinLitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
